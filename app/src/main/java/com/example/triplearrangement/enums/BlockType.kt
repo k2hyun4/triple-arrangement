39f3e04da15f18b46c756878e91e9ee4a21dacd8
k2hyun4 <k2hyun4@naver.com>
2020-12-11 00:19:29 +0900

    feat : 블럭 유형 추가(high_{red, blue, yellow, green}
    - orange, purple 눈 모양 변경

--- app/src/main/java/com/example/triplearrangement/enums/BlockType.kt
+++ app/src/main/java/com/example/triplearrangement/enums/BlockType.kt
@@ -9,5 +9,9 @@ enum class BlockType(val resourceId: Int, val selectedResourceId:Int, val level:
     GREEN(R.drawable.block_green, R.drawable.block_green_selected, 3),
     ORANGE(R.drawable.block_orange, R.drawable.block_orange_selected, 4),
     PURPLE(R.drawable.block_purple, R.drawable.block_purple_selected,  5),
-    WHITE(R.drawable.block_white, R.drawable.block_white_selected, 6);
+    WHITE(R.drawable.block_white, R.drawable.block_white_selected, 6),
+    HIGH_RED(R.drawable.block_high_red, R.drawable.block_high_red_selected, 7),
+    HIGH_BLUE(R.drawable.block_high_blue, R.drawable.block_high_blue_selected, 8),
+    HIGH_YELLOW(R.drawable.block_high_yellow, R.drawable.block_high_yellow_selected, 9),
+    HIGH_GREEN(R.drawable.block_high_green, R.drawable.block_high_green_selected, 10);
 }
